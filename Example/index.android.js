44bced8ef598a024522808b3839d28cef5bc0099
Vinay singh <vinaysinghsatna01@gmail.com>
2017-08-04 00:54:40 +0530

    Update Index view for android in example project

--- Example/index.android.js
+++ Example/index.android.js
@@ -9,22 +9,63 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import CachedVideo from 'react-native-cached-video';
+import VideoPlayer from 'react-native-video-player';
+
 
 export default class TestProject extends Component {
+  constructor() {
+    super();
+    this.state ={
+      url: '',
+      uri: 'http://www.sample-videos.com/video/mp4/480/big_buck_bunny_480p_1mb.mp4',
+    };
+    this.testUri = this.testUri.bind(this);
+  }
+
+  testUri() {
+    this.state.url.includes('http:') || this.state.url.includes('https') ?
+      this.setState({ uri: this.state.url }) : alert('This is not valid url')
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <CachedVideo
-          style={{ width: 300, height: 300, backgroundColor: 'grey' }}
-          source={{ uri: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4' }}
-          thumbnail={{ uri: 'http://www.iconsdb.com/icons/preview/black/play-xxl.png' }}
-        />
+        <ScrollView>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Cached video Example
         </Text>
+        <Text style={styles.instructions}>
+         Enter a valid url Click on test button
+        </Text>
+        <TextInput
+          onChangeText={(url) => this.setState({url})}
+          placeholder={'Enter video url'}
+          placeholderTextColor={'grey'}
+          style={styles.textInput}
+          underlineColorAndroid={'transparent'}
+        />
+
+        <TouchableOpacity
+          onPress={()=> this.testUri()}
+          style = {styles.button}
+        >
+          <Text style={[styles.welcome, { color: 'red' }]}>
+              Test
+          </Text>
+        </TouchableOpacity>
+
+        <CachedVideo
+          style={styles.video}
+          source={{ uri: this.state.uri }}
+        />
+        </ScrollView>
       </View>
     );
   }
@@ -37,6 +78,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  textInput: {
+    margin: 10,
+    height: 50,
+    borderColor: 'grey',
+    alignSelf: 'stretch',
+    borderWidth: 1,
+  },
+  button: {
+    borderWidth: 1,
+    margin: 10,
+    alignSelf: 'stretch',
+    height: 40,
+    borderColor: 'red',
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
@@ -46,6 +104,17 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  video: {
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5,
+    width: 300,
+    height: 300,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
